da540f1bcb93afef1af904d0347ee8a3272d59f8
Cathella <ncathie214@gmail.com>
2021-11-20 05:26:12 +0300

    Remmove linters

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const close = document.querySelector('.close');
+const menuBars = document.querySelector('.menu-bars');
+
 function showMobileMenu() {
   const menu = document.querySelector('#mobile-nav');
-  menu.style.display = "block";
+  menu.style.display = 'block';
 } 
 
 function hideMobileMenu() {
   const close = document.querySelector('#mobile-nav');
-  close.style.display = "none";
-} 
+  close.style.display = 'none';
+}
+
+menuBars.addEventListener('click', showMobileMenu());
+close.addEventListener('click', hideMobileMenu());
