ce7e8869b32d7b804ad8ef798e74665692ac28f9
Cathella <ncathie214@gmail.com>
2021-11-20 05:48:44 +0300

    App js updates

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const close = document.querySelector('.close');
 const menuBars = document.querySelector('.menu-bars');
+const menu = document.querySelector('#mobile-nav');
 
 function showMobileMenu() {
-  const menu = document.querySelector('#mobile-nav');
   menu.style.display = 'block';
 }
 
 function hideMobileMenu() {
-  const close = document.querySelector('#mobile-nav');
-  close.style.display = 'none';
+  // menu.style.display = 'none';
 }
 
 menuBars.addEventListener('click', showMobileMenu());
