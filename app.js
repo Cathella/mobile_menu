d618d994e44a28550517ce66e895b2cd4b3dafe1
Cathella <ncathie214@gmail.com>
2021-11-20 05:28:49 +0300

    Remove linter errors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const menuBars = document.querySelector('.menu-bars');
 function showMobileMenu() {
   const menu = document.querySelector('#mobile-nav');
   menu.style.display = 'block';
-} 
+}
 
 function hideMobileMenu() {
   const close = document.querySelector('#mobile-nav');
